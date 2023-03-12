41ea4d3ed9e07cdc812d3ea4137d3bb839bc616b
Surya-2811 <84892906+surya-2811@users.noreply.github.com>
2023-03-12 16:51:16 +0530

    added show/hide test cases for input component

--- jotto/src/Input.js
+++ jotto/src/Input.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function Input({ secretWord }) {
+function Input({ success, secretWord }) {
   const [currentGuess, setCurrentGuess] = React.useState("")
+  if(success){
+    return <div data-test='input-component'></div>
+  }
   return (
     <div data-test='input-component'>
       <from className="form-inline">
@@ -19,6 +22,10 @@ function Input({ secretWord }) {
         <button
           className='btn btn-primary mb-2'
           data-test="submit-button"
+          onClick={(event)=>{
+            event.preventDefault();
+            setCurrentGuess("")
+          }}
         >Submit</button>
 
       </from>
