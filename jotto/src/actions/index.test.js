5c84d4080316e35d2ec1b457dbfaab6ae3826dee
Surya-2811 <84892906+surya-2811@users.noreply.github.com>
2023-03-13 15:33:01 +0530

    added functional test

---
+++ jotto/src/actions/index.test.js
@@ -0,0 +1,27 @@
+import moxios from "moxios";
+import { getSecretWord } from ".";
+
+describe('getSecretWord', ()=>{
+    beforeEach(()=>{
+        moxios.install();
+
+    })
+    afterEach(()=>{
+        moxios.uninstall();
+
+    })
+
+    test('secretWord is returned', ()=>{
+        moxios.wait(()=>{
+            const request = moxios.requests.mostRecent();
+            request.respondWith({
+                status: 200,
+                response: 'party'
+            })
+        })
+        return getSecretWord()
+        .then((secretWord)=>{{
+            expect(secretWord).toBe('party')
+        }})
+    })
+})
